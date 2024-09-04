6f301eaaeaa922e019ae0e43d09cc65a0dd3b58e
shinobi011 <shubham_ug21@nsut.ac.in>
2024-09-04 19:19:00 +0530

    added path finding visualizer

---
+++ public/browser/Maze Algorithm/weightsDemonstration.js
@@ -0,0 +1,33 @@
+// This weightsDemonstration function modifies the status and weight of nodes in a grid-based board,
+// creating specific patterns of walls and weighted nodes
+
+function weightsDemonstration(board) {
+    let currentIdX = board.height - 1;
+    let currentIdY = 35;
+    while (currentIdX > 5) {
+      let currentId = `${currentIdX}-${currentIdY}`;
+      let currentElement = document.getElementById(currentId);
+      board.wallsToAnimate.push(currentElement);
+      let currentNode = board.nodes[currentId];
+      currentNode.status = "wall";
+      currentNode.weight = 0;
+      currentIdX--;
+    }
+    for (let currentIdX = board.height - 2; currentIdX > board.height - 11; currentIdX--) {
+      for (let currentIdY = 1; currentIdY < 35; currentIdY++) {
+        let currentId = `${currentIdX}-${currentIdY}`;
+        let currentElement = document.getElementById(currentId);
+        board.wallsToAnimate.push(currentElement);
+        let currentNode = board.nodes[currentId];
+        if (currentIdX === board.height - 2 && currentIdY < 35 && currentIdY > 26) {
+          currentNode.status = "wall";
+          currentNode.weight = 0;
+        } else {
+          currentNode.status = "unvisited";
+          currentNode.weight = 15;
+        }
+      }
+    }
+  }
+  
+  module.exports = weightsDemonstration;
